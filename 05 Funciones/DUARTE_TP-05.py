a8ed3924be5dfbdbb29fbe4653ac979b1a121d7c
FedeDuarte1992 <124315674+FedeDuarte1992@users.noreply.github.com>
2025-04-13 02:10:28 -0300

    entrega tp5

--- 05 Funciones/DUARTE_TP-05.py
+++ 05 Funciones/DUARTE_TP-05.py
@@ -1,27 +1,29 @@
 """1. Crear una función llamada imprimir_hola_mundo que imprima por
 pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el
-programa principal.
+programa principal."""
 #Definición de funciones  (serán llamadas en cualquier parte del archivo de ser necesarias, pero se crean en cada ejercicio en particular)
 def imprimir_hola_mundo():
     print("Hola mundo!")
 #Programa
-imprimir_hola_mundo()"""
-"""2. Crear una función llamada saludar_usuario(nombre) que reciba
+imprimir_hola_mundo()
+"""///////////////////////////////////////////////////////////
+2. Crear una función llamada saludar_usuario(nombre) que reciba
 como parámetro un nombre y devuelva un saludo personalizado.
 Por ejemplo, si se llama con saludar_usuario("Marcos"), deberá de-
 volver: “Hola Marcos!”. Llamar a esta función desde el programa
-principal solicitando el nombre al usuario.
+principal solicitando el nombre al usuario."""
 #Definición de funciones  (serán llamadas en cualquier parte del archivo de ser necesarias, pero se crean en cada ejercicio en particular)
 def saludar_usuario(nombre):
     print (f"Hola {nombre}!")
 #Programa
 nombre = input("Ingrese su nombre de usuario: ")
-saludar_usuario(nombre)"""
-"""3. Crear una función llamada información_personal(nombre, apellido,
+saludar_usuario(nombre)
+"""/////////////////////////////////////////////////////////////////
+3. Crear una función llamada información_personal(nombre, apellido,
 edad, residencia) que reciba cuatro parámetros e imprima: “Soy
 [nombre] [apellido], tengo [edad] años y vivo en [residencia]”. Pe-
 dir los datos al usuario y llamar a esta función con los valores in-
-gresados.
+gresados."""
 #Definición de funciones  (serán llamadas en cualquier parte del archivo de ser necesarias, pero se crean en cada ejercicio en particular)
 def información_personal(nombre,apellido,edad,residencia):
     print(f"soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
@@ -31,12 +33,13 @@ apellido = input("Ingrese su apellido: ")
 edad = int(input("Ingrese su edad: "))
 residencia = input("Ingrese su residencia: ")
 información_personal(nombre,apellido,edad,residencia)
-"""
-"""4. Crear dos funciones: calcular_area_circulo(radio) que reciba el ra-
+
+"""///////////////////////////////////////////////////////////////////
+4. Crear dos funciones: calcular_area_circulo(radio) que reciba el ra-
 dio como parámetro y devuelva el área del círculo. calcular_peri-
 metro_circulo(radio) que reciba el radio como parámetro y devuel-
 va el perímetro del círculo. Solicitar el radio al usuario y llamar am-
-bas funciones para mostrar los resultados.
+bas funciones para mostrar los resultados."""
 #Definición de funciones  (serán llamadas en cualquier parte del archivo de ser necesarias, pero se crean en cada ejercicio en particular)
 import math #Se importa de la biblioteca para usar pi
 def calcular_area_circulo(radio):
@@ -46,22 +49,25 @@ def calcular_perimetro_circulo(radio):
 #Programa
 pi = math.pi
 radio=float(input("Ingrese el radio: "))
-print (f"el area del circulo es {calcular_area_circulo(radio)} y el perimetro es {calcular_perimetro_circulo(radio)}")"""
-"""5. Crear una función llamada segundos_a_horas(segundos) que reciba
+print (f"el area del circulo es {calcular_area_circulo(radio)} y el perimetro es {calcular_perimetro_circulo(radio)}")
+
+"""///////////////////////////////////////////////////////////////////
+5. Crear una función llamada segundos_a_horas(segundos) que reciba
 una cantidad de segundos como parámetro y devuelva la cantidad
 de horas correspondientes. Solicitar al usuario los segundos y mos-
-trar el resultado usando esta función.
+trar el resultado usando esta función."""
 #Definición de funciones  (serán llamadas en cualquier parte del archivo de ser necesarias, pero se crean en cada ejercicio en particular)
 def segundos_a_horas(segundos):
-    return segundos // 3600
+    return segundos / 3600
 #Programa
 segundos=int(input("Ingrese la cantidad de segundos: "))
 print(f"La cantidad de segundos en horas es {segundos_a_horas(segundos)}")
-"""
-"""6. Crear una función llamada tabla_multiplicar(numero) que reciba un
+
+"""///////////////////////////////////////////////////////////////////
+6. Crear una función llamada tabla_multiplicar(numero) que reciba un
 número como parámetro y imprima la tabla de multiplicar de ese
 número del 1 al 10. Pedir al usuario el número y llamar a la fun-
-ción.
+ción."""
 #Definición de funciones  (serán llamadas en cualquier parte del archivo de ser necesarias, pero se crean en cada ejercicio en particular)
 def tabla_multiplicar(numero):
     for i in range(1,11):
@@ -69,12 +75,13 @@ def tabla_multiplicar(numero):
         print(f"{i} X {numero}= {resultado}")
 #Programa
 numero =int(input("ingrese un numero para ver su tabla: "))
-tabla_multiplicar(numero)"""
+tabla_multiplicar(numero)
 
-"""7. Crear una función llamada operaciones_basicas(a, b) que reciba
+"""///////////////////////////////////////////////////////////////////
+7. Crear una función llamada operaciones_basicas(a, b) que reciba
 dos números como parámetros y devuelva una tupla con el resulta-
 do de sumarlos, restarlos, multiplicarlos y dividirlos. Mostrar los re-
-sultados de forma clara.
+sultados de forma clara."""
 #Definición de funciones  (serán llamadas en cualquier parte del archivo de ser necesarias, pero se crean en cada ejercicio en particular)
 def operaciones_basicas(a, b):
     suma = a + b
@@ -90,12 +97,13 @@ suma, resta,multiplicacion, division = resultados
 print(f"la suma de ambos números es: {suma}")
 print(f"la resta de ambos números es: {resta}")
 print(f"la multiplicacion de ambos números es: {multiplicacion}")
-print(f"la division de ambos números es: {division}")"""
+print(f"la division de ambos números es: {division}")
 
-"""8. Crear una función llamada calcular_imc(peso, altura) que reciba el
+"""///////////////////////////////////////////////////////////////////
+8. Crear una función llamada calcular_imc(peso, altura) que reciba el
 peso en kilogramos y la altura en metros, y devuelva el índice de
 masa corporal (IMC). Solicitar al usuario los datos y llamar a la fun-
-ción para mostrar el resultado con dos decimales.
+ción para mostrar el resultado con dos decimales."""
 #Definición de funciones  (serán llamadas en cualquier parte del archivo de ser necesarias, pero se crean en cada ejercicio en particular)
 def calcular_imc(peso, altura):
     altura_m=altura /100
@@ -105,24 +113,24 @@ def calcular_imc(peso, altura):
 peso=float(input("Ingrese su peso en Kg: "))
 altura=float(input("Ingrese su altura en cm : "))
 resultado_imc=calcular_imc(peso,altura)
-print(f"El IMC es {resultado_imc}")"""
+print(f"El IMC es {resultado_imc}")
 
-"""9. Crear una función llamada celsius_a_fahrenheit(celsius) que reciba
+"""///////////////////////////////////////////////////////////////////
+9. Crear una función llamada celsius_a_fahrenheit(celsius) que reciba
 una temperatura en grados Celsius y devuelva su equivalente en
 Fahrenheit. Pedir al usuario la temperatura en Celsius y mostrar el
-resultado usando la función.
+resultado usando la función."""
 #Definición de funciones  (serán llamadas en cualquier parte del archivo de ser necesarias, pero se crean en cada ejercicio en particular)
 def celsius_a_fahrenheit(celsius):
     return  (9/5)*celsius + 32
-    
-    
 #Programa
 celsius=float(input(" Ingrese la temperatura en grados Celsius: "))
 grados_fahrenheit=celsius_a_fahrenheit(celsius)
-print(f"la conversion de {celsius} grados celsius  a grados fahrenheit es {grados_fahrenheit}F ")"""
+print(f"la conversion de {celsius} grados celsius  a grados fahrenheit es {grados_fahrenheit}F ")
 
 
-"""10.Crear una función llamada calcular_promedio(a, b, c) que reciba
+"""///////////////////////////////////////////////////////////////////
+10.Crear una función llamada calcular_promedio(a, b, c) que reciba
 tres números como parámetros y devuelva el promedio de ellos.
 Solicitar los números al usuario y mostrar el resultado usando esta
 función.
